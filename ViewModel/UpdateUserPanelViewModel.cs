b7dfb990173cf7b40f62b27911f4ae7200d4e220
arshiadousti <70818876+arshiadousti@users.noreply.github.com>
2021-02-08 22:33:54 +0330

    Add files via upload

---
+++ ViewModel/UpdateUserPanelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.ViewModel
+{
+    public class UpdateUserPanelViewModel
+    {
+        [Display(Name = "نام و نام خانوادگی")]
+        public string FullName { get; set; }
+    }
+}
